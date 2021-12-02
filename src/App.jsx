33fb87d49b424a956326d783b5f685cc95ceba15
Yan Dvornitskyi <dvornitskyi@gmail.com>
2021-12-02 23:22:35 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+  const addTodo = () => {
+    if(text.trim().length){
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed:false,
+        }
+      ])
+      setText('');
+    }
+  }
+  const removeTodo = (todoId) => {
+    setTodos(todos.filter(todo => todo.id !== todoId))
+  }
+  const toggleTodoCompleted = (todoId) => {
+    setTodos(
+      todos.map(
+        todo => {
+          if(todo.id !== todoId ) return todo;
+            return{
+            ...todo,
+            completed: !todo.completed,
+            }
+        }
+    )
+    );
+  }
+  return (
+    <div className="App">
+      <label>
+        <input value={text} onChange={(e) => setText(e.target.value)}/>
+        <button onClick={addTodo}>Add Todo</button>
+      </label>
+      <ul>
+        {
+          todos.map (todo => <li key={todo.id}>
+            <input type="checkbox" checked={todo.completed} onChange={() => toggleTodoCompleted(todo.id)}/>
+            <span>{todo.text}</span>
+            <span className="delete" onClick={() => removeTodo(todo.id)}>&times;</span>
+          </li>)
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
