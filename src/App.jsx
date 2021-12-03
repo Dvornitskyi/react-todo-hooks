7b5ba76299e3bc5a3306c79bd879f8a119beb266
Yan Dvornitskyi <dvornitskyi@gmail.com>
2021-12-03 10:19:01 +0200

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import {useState} from 'react';
+import TodoList from './components/TodoList';
+import InputField from './components/InputField';
 import './App.css';
 
 function App() {
@@ -35,19 +37,16 @@ function App() {
   }
   return (
     <div className="App">
-      <label>
-        <input value={text} onChange={(e) => setText(e.target.value)}/>
-        <button onClick={addTodo}>Add Todo</button>
-      </label>
-      <ul>
-        {
-          todos.map (todo => <li key={todo.id}>
-            <input type="checkbox" checked={todo.completed} onChange={() => toggleTodoCompleted(todo.id)}/>
-            <span>{todo.text}</span>
-            <span className="delete" onClick={() => removeTodo(todo.id)}>&times;</span>
-          </li>)
-        }
-      </ul>
+      <InputField 
+        text={text} 
+        handleInput={setText}
+        handleSubmit={addTodo}
+      />
+      <TodoList 
+        todos={todos}
+        toggleTodoCompleted={toggleTodoCompleted}
+        removeTodo={removeTodo}
+      />
     </div>
   );
 }
